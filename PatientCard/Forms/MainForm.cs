0bac659854bfbfbddfe3313a32dbd04c9dbd540e
unknown <Sergey.Svirsky@wsa-008.icx.local>
2014-06-26 11:46:33 +0300

    Init commit

---
+++ PatientCard/Forms/MainForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PatientCard.Forms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var form = new HistoryForm();
+            form.Show(this);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var form = new PatientCardForm();
+            form.Show(this);
+        }
+
+    }
+}
