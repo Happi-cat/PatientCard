57c439610b9422c3ff7ac70198abfca5356f974d
Sergey Svirsky <zversergey@tut.by>
2014-10-10 11:40:34 +0300

    more styles

--- PatientCard.Web/Content/patient-card/modules/directives/myTableHead.js
+++ PatientCard.Web/Content/patient-card/modules/directives/myTableHead.js
@@ -9,6 +9,29 @@ angular.module('patient-card.directives')
 				sortFields: '=mySortFields'
 			},
 			templateUrl: '/Widget/TableHeader',
-			controller: TableHeadCtrl
+			controller: function ($scope) {
+				$scope.toogleSort = function (item) {
+					if (item.sort == 1) {
+						item.sort = -1;
+					} else if (item.sort == -1) {
+						item.sort = 0;
+					} else {
+						item.sort = 1;
+					}
+
+					if (!$scope.sortFields || !$scope.tableHead)
+						return;
+
+					$scope.sortFields.splice(0, $scope.sortFields.length);
+					angular.forEach($scope.tableHead, function (column) {
+						if (column.sort == 1) {
+							$scope.sortFields.push(column.name);
+						} else if (column.sort == -1) {
+							$scope.sortFields.push('-' + column.name);
+						}
+					});
+
+				};
+			}
 		};
 	});
