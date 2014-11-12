36086742adc02be4ff21dd4375269aabcaae4a8d
Sergey Svirsky <zversergey@tut.by>
2014-11-12 14:37:47 +0300

    models

--- server/components/express-model/index.js
+++ server/components/express-model/index.js
@@ -2,13 +2,36 @@
 
 var _ = require('lodash');
 var Cop = require('./../entity-cop');
+var Repos = require('./../entity-repos');
 
 module.exports = function (name, schema) {
 	var cop = new Cop(schema.fields);
+	var repos = new Repos(schema);
 
 	return function (req, res, next) {
-		req.cops = req.cops || {};
-		req.cops[name] = cop;
+		req.models = req.models || {};
+
+		req.models[name] = {
+			validate: function (obj) {
+				return cop.validate(obj);
+			},
+			find: function (where, done) {
+				return repos.find(req, where, done);
+			},
+			findOne: function (where, done) {
+				return repos.findOne(req, where, done);
+			},
+			create: function (obj, done) {
+				return repos.create(req, obj, done);
+			},
+			update: function (where ,obj, done) {
+				return repos.update(req, where, obj, done);
+			},
+			delete: function (where, done) {
+				return repos.delete(req, where, done);
+			}
+		};
+
 		next();
 	};
 }
--- server/models/index.js
+++ server/models/index.js
@@ -3,14 +3,14 @@
 var model = require('./../components/express-model');
 
 module.exports = function (app) {
-	app.use(model('patient', require('./patient/patient.schema')));
-	app.use(model('patientFirstSurvey', require('./patient-first-survey/patient-first-survey.schema')));
-	app.use(model('patientStatusCpi', require('./patient-status-cpi/patient-status-cpi.schema')));
-	app.use(model('patientStatusDentist', require('./patient-status-dentist/patient-status-dentist.schema')));
-	app.use(model('patientStatusDfm', require('./patient-status-dfm/patient-status-dfm.schema')));
-	app.use(model('patientStatusOhis', require('./patient-status-ohis/patient-status-ohis.schema')));
-	app.use(model('patientSurvey', require('./patient-survey/patient-survey.schema')));
-	app.use(model('patientTreatmentPlan', require('./patient-treatment-plan/patient-treatment-plan.schema')));
-	app.use(model('patientVisit', require('./patient-visit/patient-visit.schema')));
-	app.use(model('user', require('./user/user.schema')));
+	app.use(model('patient', require('./patient')));
+	app.use(model('patientFirstSurvey', require('./patient-first-survey')));
+	app.use(model('patientStatusCpi', require('./patient-status-cpi')));
+	app.use(model('patientStatusDentist', require('./patient-status-dentist')));
+	app.use(model('patientStatusDfm', require('./patient-status-dfm')));
+	app.use(model('patientStatusOhis', require('./patient-status-ohis')));
+	app.use(model('patientSurvey', require('./patient-survey')));
+	app.use(model('patientTreatmentPlan', require('./patient-treatment-plan')));
+	app.use(model('patientVisit', require('./patient-visit')));
+	app.use(model('user', require('./user')));
 }
