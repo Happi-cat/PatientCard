d094880aa96a2ad55e7262834a152dffff12c600
Sergey Svirsky <zversergey@tut.by>
2014-11-21 16:27:38 +0300

    /user/me endpoint

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -2,9 +2,12 @@
 
 var express = require('express');
 var controller = require('./user.controller');
+var auth = require('./../../auth/auth.service');
 
 var router = express.Router();
 
-router.get('/', controller.index);
+router.get('/', auth.hasRole('administrator'), controller.index);
+router.get('/me', auth.isAuthenticated(), controller.me);
+router.get('/:username', auth.hasRole('administrator'),  controller.item);
 
 module.exports = router;
--- server/api/user/user.controller.js
+++ server/api/user/user.controller.js
@@ -6,5 +6,26 @@ var User =  require('./user.model');
 
 // Get list of users
 exports.index = function(req, res) {
-  res.json([]);
+  	User.find(req, function (err, data) {
+		if (err) return next(err);
+		return res.json(data);
+	})
+};
+
+exports.me = function(req, req) {
+	User.findOne(req, {
+		username: req.user.username
+	}, function (err, data) {
+		if (err) return next(err);
+		return res.json(data);
+	})
+};
+
+exports.item = function(req, req) {
+	User.findOne(req, {
+		username: req.params.username
+	}, function (err, data) {
+		if (err) return next(err);
+		return res.json(data);
+	})
 };
